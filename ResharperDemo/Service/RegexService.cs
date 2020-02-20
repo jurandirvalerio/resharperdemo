4c5b6a793987f07d4c0fc756fb795ae1f7c65169
Jurandir Valerio <jjunior@prima.com.br>
2020-02-20 15:41:13 -0300

    Regex.

---
+++ ResharperDemo/Service/RegexService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Service
+{
+	class RegexService
+	{
+		void Teste()
+		{
+			// Ponteiro na expressão - Validate Regular Expression
+			Regex.Match("10/05/1988", @"\d{2}\/\d{2}/\d{4}");
+		}
+	}
+}
