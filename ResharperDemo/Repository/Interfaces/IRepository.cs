9c9bab673fabb8cb840a1fb033a847f075fc7108
Jurandir Valerio <jjunior@prima.com.br>
2020-02-20 14:04:28 -0300

    Refatoração.

--- ResharperDemo/Repository/Interfaces/IRepository.cs
+++ ResharperDemo/Repository/Interfaces/IRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Repository.Interfaces
 {
+	// modificador inconsistente
 	interface IRepository
 	{
 	}
---
+++ ResharperDemo/Service/RefatoracaoImcService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Service
+{
+	public class RefatoracaoImcService
+	{
+		public float CalcularImc(float peso, float altura)
+		{
+			// ctrl + shift + r 
+			// inline method
+			float quadradoAltura = altura * altura;
+			return peso / quadradoAltura;
+		}
+	}
+}
