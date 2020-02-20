cb8a3a66993ff3a78099769b822a44f7d86d313b
Jurandir Valerio <jjunior@prima.com.br>
2020-02-20 14:21:06 -0300

    Extrair classe

---
+++ ResharperDemo/Domain/Pessoa.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+	class Pessoa
+	{
+		public int Nome { get; set; }
+		public DateTime DataAniversario { get; set; }
+		// Extrair classe
+		public string Endereco { get; set; }
+		public string Cidade { get; set; }
+	}
+}
